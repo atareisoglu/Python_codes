3a996a7e275dc994c10df03ff27ec8300f81acb2
atareisoglu <81226649+atareisoglu@users.noreply.github.com>
2023-12-18 20:56:13 +0300

    Add files via upload

---
+++ variables_deneme.py
@@ -0,0 +1,16 @@
+musteriAdi = 'Ali'
+musteriSoyad = 'Yılmaz'
+musteriAdSoyad = musteriAdi + ' ' + musteriSoyad
+musteriCinsiyet = True #Erkek
+musteriTC = '12312535773'
+musteriDogumYili = 1989
+musteriAdresi = 'İstanbul Kadıköy'
+musteriYası = 2020 - musteriDogumYili
+
+order1 = 110
+order2 = 1100.5
+order3 = 365.95
+
+total = order1 + order2 + order3
+
+print('Sayın:', musteriAdSoyad, total, 'tutarındaki siparişiniz', musteriAdresi, 'adresine kargoılanmıştır.')
